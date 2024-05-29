49ee10bf77fe879b2bcb0b3f803dfb0dd1304b1d
Midhun Sankar P <94545296+midhunsankarp@users.noreply.github.com>
2024-05-29 09:55:39 +0530

    ADD material_issue

---
+++ machinser_assessment/material_issue/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Material Issue',
+    'version': '17.0.1.0.0',
+    'summary': 'Material Issue',
+    'description': """Material Issue
+        """,
+    'category': 'Inventory/Inventory',
+    'depends': ['stock', 'account_accountant'],
+    'data': [
+        'views/stock_picking_views.xml'
+    ],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
---
+++ machinser_assessment/material_issue/models/stock_picking.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from odoo import fields, models
+
+
+class StockPicking(models.Model):
+    _inherit = 'stock.picking'
+
+    is_material_issue = fields.Boolean(string="Material request",
+                                       help="Enable this field to add issue "
+                                            "product.",
+                                       default=False)
+    issue_product_id = fields.Many2one(comodel_name="product.product",
+                                       help="Once selected, the move-line "
+                                            "products will be affected with "
+                                            "issue product expense account.",
+                                       string="Issue Product")
+
+    def button_validate(self):
+        if self.is_material_issue and \
+                self.issue_product_id.property_account_expense_id:
+            for move in self.move_ids:
+                move.product_id.property_account_expense_id = \
+                    self.issue_product_id.property_account_expense_id
+        return super().button_validate()
